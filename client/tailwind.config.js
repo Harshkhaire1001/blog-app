2a44d51a6d88994e218af0b4816aa2e9e311ff98
HarshKhaire <harshkhaire1001@gmail.com>
2024-02-20 12:10:07 +0530

    create header section 1

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,9 +2,10 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx}",
+    "node_modules/flowbite-react/lib/esm/**/*.js",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 }
