4499a3eef50d1c36bd3af1e6986a9c6d73d6a4c8
HarshKhaire <harshkhaire1001@gmail.com>
2024-02-28 00:02:16 +0530

    delete user functionality to admin user

--- client/src/components/DashUsers.jsx
+++ client/src/components/DashUsers.jsx
@@ -54,8 +54,22 @@ export default function DashUsers() {
     }
   };
 
-  const handleDeleteUser = () => {
-
+  const handleDeleteUser = async () => {
+    try {
+      const res = await fetchWithBaseURL(`api/user/delete/${userIdToDelete}`, {
+        method: 'DELETE',
+        credentials: 'include',
+      });
+      const data = await res.json();
+      if (res.ok) {
+        setUsers( (prev) => prev.filter( (user) => user._id !== userIdToDelete));
+        setShowModal(false);
+      } else {
+        console.log(data.message);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
   }
 
   return (
