c9b03f2975b3ed2292319c859c51bff6ca98f241
HarshKhaire <harshkhaire1001@gmail.com>
2024-02-29 19:13:39 +0530

    show comments of the post

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -92,7 +92,7 @@ const handleDeletePost = async () => {
             </Table.HeadCell>
           </Table.Head>
           {userPosts.map( (post) => (
-            <Table.Body className='divide-y'>
+            <Table.Body className='divide-y' key={post._id}>
               <Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800'>
                 
                 <Table.Cell>
